261de8b1d5030966c17f9ef3830caa57ba22478a
AyushVyas8080 <ayushvyas909@gmail.com>
2026-01-07 14:22:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function updateClock() {
+  const date = new Date();
+
+  let hours = date.getHours();
+  hours = hours % 12 || 12;
+  hours = hours.toString().padStart(2, 0);
+
+  const meridian = hours >= 12 ? 'PM' : 'AM';
+  const minutes = date.getMinutes().toString().padStart(2, 0);
+  const seconds = date.getSeconds().toString().padStart(2, 0);
+
+  const timeText = `${hours}:${minutes}:${seconds} ${meridian}`;
+
+  const clock = document.getElementById('clock');
+  clock.innerText = timeText;
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
