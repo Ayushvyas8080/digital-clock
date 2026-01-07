44ed5f3877ceb01944613776f032435905d470b5
Ayushvyas8080 <ayushvyas909@gmail.com>
2026-01-07 14:52:55 +0530

    Fix duplicate declaration of meridian variable

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function updateClock() {
   const date = new Date();
 
   let hours = date.getHours();
+  const meridian = hours >= 12 ? 'PM' : 'AM';
   hours = hours % 12 || 12;
   hours = hours.toString().padStart(2, 0);
 
-  const meridian = hours >= 12 ? 'PM' : 'AM';
+  
   const minutes = date.getMinutes().toString().padStart(2, 0);
   const seconds = date.getSeconds().toString().padStart(2, 0);
 
